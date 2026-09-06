c99b1e4537e0e4c387d079e61d3263b93494c4d3
Wallison Henrique <wallisonhen@gmail.com>
2026-09-06 07:06:01 -0300

    feat: add number field component

---
+++ frontend/apps/cart-web-vite/src/components/NumberField/index.tsx
@@ -0,0 +1,49 @@
+import "./styles.css"
+
+interface Props {
+    value: number
+    min?: number
+    max?: number
+    step?: number
+    onChange: (value: number) => void
+}
+
+function NumberField({value, min = 0, max = 99, step = 1, onChange}: Props) {
+    const handleValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+        let result = Number(e.target.value)
+
+        if (result < min) result = min
+        if (result > max) result = max
+
+        onChange(result)
+    }
+
+    return (
+        <div className="number-field">
+            <button 
+                className="number-field-controls"
+                type="button"
+                disabled={value === min}
+                onClick={() => onChange(value - step)}
+            >
+                -
+            </button>
+            <input 
+                className="number-field-input" 
+                value={value} 
+                type="text"
+                onChange={handleValue} 
+            />
+            <button 
+                className="number-field-controls"
+                type="button"
+                disabled={value === max} 
+                onClick={() => onChange(value + step)}
+            >
+                +
+            </button>
+        </div>
+    )
+}
+
+export default NumberField
