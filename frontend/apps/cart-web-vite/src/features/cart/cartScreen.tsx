15e92ac6dc22f3d552bfc41b368c5e1389c09452
Wallison Henrique <wallisonhen@gmail.com>
2026-09-11 12:55:46 -0300

    feat: add cart screen

--- frontend/apps/cart-web-vite/src/features/cart/cartScreen.tsx
+++ frontend/apps/cart-web-vite/src/features/cart/cartScreen.tsx
@@ -1,5 +1,35 @@
+import CartItem from "../../components/cart-item"
+
+const PHOTO = "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcR1ieevip1Dtt_koA21ZPais5TCTphFsAS8s_Qd0ETjE1D2FZeWWqRS5sqiFM3rzF7U40RvrluU2V3-w7yn1rUfDyi0hBwAB-NgLwwKRkrL2IGolPz8uNqVKg"
+const CART = [
+    { id: 1, photo: PHOTO, name: "Macbook Pro", price: 8705.21, qtd: 2 },
+    { id: 2, photo: PHOTO, name: "Macbook Pro 3", price: 10145.58, qtd: 5 },
+    { id: 3, photo: PHOTO, name: "Macbook Pro 2", price: 9157.84, qtd: 1 }
+]
+
 function CartScreen() {
-    return  "CartScreen"
+    const handleEditCart = (action: string, id: number) => alert(`${action} ${id}`)
+
+    const {count, list} = CART.reduce<{
+        count: number
+        list: React.ReactElement[]
+    }>((acc, current) => {
+        const cartItem = (
+            <CartItem
+                product={current}
+                onClick={handleEditCart}
+            />
+        )
+
+        return {
+            count: acc.count + current.qtd,
+            list: [...acc.list, cartItem]
+        }
+    }, {count: 0, list: []})
+
+    return (
+        <div>{list}</div>
+    )
 }
 
 export default CartScreen
