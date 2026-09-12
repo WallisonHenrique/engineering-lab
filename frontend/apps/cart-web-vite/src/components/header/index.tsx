ff178f07e9bdc00ad5a9f3993d70c9eddcd677b5
Wallison Henrique <wallisonhen@gmail.com>
2026-09-12 00:25:50 -0300

    feat(mini-cart) add view cart in summary

--- frontend/apps/cart-web-vite/src/components/header/index.tsx
+++ frontend/apps/cart-web-vite/src/components/header/index.tsx
@@ -4,19 +4,32 @@ import MiniCart from '../mini-cart';
 import './styles.css';
 import Menu from '../menu';
 import { useCart } from '@/contexts';
+import { Button } from '@/components/Button';
+import { Link } from '@tanstack/react-router';
+import { TotalPrice } from '@/components/TotalPrice';
 
 function Header() {
-    const { items, totalItems } = useCart()
+    const cart = useCart()
 
     return (
         <header className='header'>
             <Menu />
             <div className='mini-cart-wrapper'>
-                <CartButtom>{totalItems}</CartButtom>
+                <CartButtom>{cart.totalItems}</CartButtom>
                 <div className='mini-cart-dropdown'>
-                    <MiniCart>{items.map(i => (
+                    <MiniCart>{cart.items.map(i => (
                         <CartItem product={i} />
-                    ))}</MiniCart>
+                    ))}
+                    <div className="mini-cart__view-cart">
+                        <div className="mini-cart__total-price">
+                            Total
+                            <TotalPrice value={cart.totalPrice} />
+                        </div>
+                        <Link to="/carrinho">
+                            <Button>Ver Carrinho</Button>
+                        </Link>
+                    </div>
+                    </MiniCart>
                 </div>
             </div>
         </header>
