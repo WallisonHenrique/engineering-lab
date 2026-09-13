a358bf56a1a08d92d83a51871e3703c1b150daf2
Wallison Henrique <wallisonhen@gmail.com>
2026-09-13 12:04:14 -0300

    refactor: adjsut and styles in product detail

--- frontend/apps/cart-web-vite/src/features/ProductDetail/ProductDetailScreen.tsx
+++ frontend/apps/cart-web-vite/src/features/ProductDetail/ProductDetailScreen.tsx
@@ -1,35 +1,47 @@
-import { ProductCard } from "@/components/ProductCard";
+import { ProductCard } from "@/components/ProductCard"
 import "./ProductDetailScreen.css"
-import { useState } from "react";
-import type { ProductModel } from "@/types/product";
-import { useProduct } from "@/hooks";
-import { TotalPrice } from "@/components/TotalPrice";
-import { Button } from "@/components/Button/Button";
-import { Link } from "@tanstack/react-router";
-import { useCart, useCartDispatch } from "@/contexts";
-import NumberField from "@/components/NumberField";
+import { useState } from "react"
+import type { ProductModel } from "@/types/product"
+import { useProduct } from "@/hooks"
+import { TotalPrice } from "@/components/TotalPrice"
+import { Button } from "@/components/Button/Button"
+import { Link } from "@tanstack/react-router"
+import { useCart, useCartDispatch } from "@/contexts"
+import NumberField from "@/components/NumberField"
+
+interface ProductDetailAddToCartBtnProps {
+    quantity: number
+    product: ProductModel
+}
+
+function ProductDetailAddToCartBtn({ quantity, product }: ProductDetailAddToCartBtnProps) {
+    const dispatch = useCartDispatch()
+    const totalPrice = product.price * quantity
+
+    const handleClick = () => 
+        dispatch({ type: "ADD_ITEM", payload: {...product, quantity}})
+
+    return (
+        <div className="product-detail__add-to-cart__btn">
+            <Button onClick={handleClick}>
+                Adicionar
+                <TotalPrice value={totalPrice} />
+            </Button>
+        </div>
+    )
+}
 
 function ProductDetailAddToCart({ product }: { product: ProductModel }) {
-    const dispatch = useCartDispatch()
     const [quantity, setQuantity] = useState(1)
-    const total = product.price * quantity
-
-    const handleQuantityChange = (value: number) => setQuantity(value)
-    const handleAddToCart = () => {
-        dispatch({ type: "ADD_ITEM", payload: {...product, quantity}})
-    }
 
     return (
         <div className="product-detail__add-to-cart">
             <NumberField 
                 value={quantity} 
                 min={1} 
-                onChange={handleQuantityChange} 
+                onChange={(value: number) => setQuantity(value)} 
             />
-            <Button onClick={handleAddToCart}>
-                Adicionar
-                <TotalPrice value={total} />
-            </Button>
+            <ProductDetailAddToCartBtn quantity={quantity} product={product} />
         </div>
     )
 }
@@ -41,21 +53,23 @@ function ProductDetailViewCart({ totalPrice }: { totalPrice: number }){
                 Total
                 <TotalPrice value={totalPrice} />
             </div>
-            <Link to="/carrinho">
-                <Button>Ver Carrinho</Button>
-            </Link>
+            <div className="product-detail__view-cart__btn">
+                <Link to="/carrinho">
+                    <Button>Ver Carrinho</Button>
+                </Link>
+            </div>
         </div>
     )
 }
 
 function ProductDetailSummary({ product }: { product: ProductModel }) {
-    const { totalPrice, getItem } = useCart()
-    const hasCart = getItem({ id: product.id })
+    const cart = useCart()
+    const hasCart = cart.getItem({ id: product.id })
 
     return (
         <div className="product-detail__summary">
             { hasCart 
-                ? <ProductDetailViewCart totalPrice={totalPrice} /> 
+                ? <ProductDetailViewCart totalPrice={cart.totalPrice} /> 
                 : <ProductDetailAddToCart product={product} />
             }
         </div>
@@ -78,6 +92,7 @@ export function ProductDetailScreen({ id }: { id: string }) {
     const { product } = useProduct({ id });
 
     if (!product) return <div>Produto não encontrado!</div>
+    
     return (
         <div className="product-detail">
             <ProductDetailItem product={product} />
