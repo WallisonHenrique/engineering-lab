4492eaa3a53e35ffd6e8d2b5ac23b74aaf9facdb
Wallison Henrique <wallisonhen@gmail.com>
2026-09-13 09:04:48 -0300

    refactor: move cart consume for header mini cart

--- frontend/apps/cart-web-vite/src/components/header/index.tsx
+++ frontend/apps/cart-web-vite/src/components/header/index.tsx
@@ -8,17 +8,16 @@ import { Button } from '@/components/Button';
 import { Link } from '@tanstack/react-router';
 import { TotalPrice } from '@/components/TotalPrice';
 
-function Header() {
+function HeaderMiniCart() {
     const cart = useCart()
 
     return (
-        <header className='header'>
-            <Menu />
-            <div className='mini-cart-wrapper'>
-                <CartButtom>{cart.totalItems}</CartButtom>
-                <div className='mini-cart-dropdown'>
-                    <MiniCart>{cart.items.map(i => (
-                        <CartItem key={i.id} product={i} />
+        <div className='header__mini-cart'>
+            <CartButtom>{cart.totalItems}</CartButtom>
+            <div className='mini-cart-dropdown'>
+                <MiniCart>
+                    { cart.items.map(i => (
+                            <CartItem key={i.id} product={i} />
                     ))}
                     <div className="mini-cart__view-cart">
                         <div className="mini-cart__total-price">
@@ -29,11 +28,19 @@ function Header() {
                             <Button>Ver Carrinho</Button>
                         </Link>
                     </div>
-                    </MiniCart>
-                </div>
+                </MiniCart>
             </div>
-        </header>
+        </div>
     )
 }
 
-export default Header
+export function Header() {
+    return (
+        <header className='header'>
+            <div className="header__menu">
+                <Menu />
+            </div>
+            <HeaderMiniCart />
+        </header>
+    )
+}
