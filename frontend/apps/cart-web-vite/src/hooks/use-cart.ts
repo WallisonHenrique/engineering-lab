808edbdab40d637e9a03f72dddc639f8a8791844
Wallison Henrique <wallisonhen@gmail.com>
2026-09-24 14:56:17 -0300

    fix(totalPrice): adjust calculate

--- frontend/apps/cart-web-vite/src/hooks/use-cart.ts
+++ frontend/apps/cart-web-vite/src/hooks/use-cart.ts
@@ -18,8 +18,8 @@ export function useCartTotalItems() {
         throw new Error('useCartTotalItems must be used within a CartProvider.');
     }
 
-    return context.allIds.reduce((acc, current) => 
-        acc + context.byId[current].quantity, 0)
+    return context.allIds.reduce((acc, id) => 
+        acc + context.byId[id].quantity, 0)
 }
 
 export function useCartTotalPrice() {
@@ -29,8 +29,10 @@ export function useCartTotalPrice() {
         throw new Error('useCartTotalPrice must be used within a CartProvider.');
     }
 
-    return context.allIds.reduce((acc, current) => 
-        acc + context.byId[current].price, 0)
+    return context.allIds.reduce((acc, id) => {
+        const current = context.byId[id]
+        return acc + current.price * current.quantity
+    }, 0)
 }
 
 export function useCartDispatch() {
